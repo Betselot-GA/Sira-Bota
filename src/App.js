ec9616fcd109cd3e679a6962b4744e5c6b157de8
Betselot-GA <betselotgetnet2227@gmail.com>
2021-11-02 09:39:19 +0300

    edited according to the figma design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,372 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <div class="container">
+            <header class="section-header">
+                <section class="header-main">
+                    <div class="container border-bottom">
+                        <div class="row align-tems-center">
+                            <div class="col-lg-8">
 
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-12">
+                                <div class="widgets-wrap float-md-right">
+
+                                    <div class="widget-header icontext">
+                                        <div class="text">
+                                            <div>
+                                                <a href="#">Sign In</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row align-items-center">
+                            <div class="col-lg-2 col-4">
+                                <a href="#" class="brand-wrap">
+                                    <a href="#" class="img-wrap"><img src="assets/images/items/siraBota_logo.png" /></a>
+                                </a>
+                            </div>
+                            <div class="col-lg-6">
+
+                            </div>
+                            <div class="col-lg-4 col-sm-6 col-12">
+                                <div class="widgets-wrap float-md-right">
+
+                                    <div class="widget-header icontext">
+                                        <div class="text">
+                                            <div>
+                                                <a href="#">List your Property</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </section>
+            </header>
+
+            <section class="section-pagetop bg">
+                <div class="row align-items-center" >
+                    <div class="col-lg-6 col-sm-6 col-6">
+                        <h2 class="title-page">Find Commercial Spaces for Rent  </h2>
+                        <p>More than 43,319 Business centers and coworking spaces in top locations</p>
+                    </div>
+                    {/* <div class="col-lg-6">
+
+                            </div> */}
+                    <div class="col-lg-4 col-sm-6 col-6">
+                        <div class="widgets-wrap float-md-right">
+
+                            <div class="widget-header icontext">
+                                <div class="text">
+                                    <div>
+                                        <a href="#" class="img-wrap"><img src="assets/images/items/working.jpg" /></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                {/* <div class="">
+                    
+                    <nav>
+
+                        <ol class="breadcrumb text-white">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Best category</a></li>
+                        </ol>
+
+                        
+                    </nav>
+
+                </div> */}
+            </section>
+            <section>
+                <div class="row align-items-center">
+                    <div class="col-lg-1 col-sm-6 h-5">
+
+                    </div>
+                    <div class="card  col-lg-8 col-sm-6 spaces">
+                        <div class="card-body">
+                            <h5 class="card-title"> Find Spaces</h5>
+                            <div class="search">
+                                <form action="#" class="search">
+
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Enter Location, Area, Buildings" />
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="submit">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-1 col-sm-6 h-5">
+
+                    </div>
+                </div>
+
+            </section>
+
+            <section class="section-content padding-y">
+                <div class="container">
+
+                    <div class="row container">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-4">
+
+                            <div class="card">
+
+                                <div class="row">
+
+                                    <div class="col">
+                                        <img class="card-img-top h-100 w-100" src="assets/images/items/office.png" alt="Card image cap" />
+                                    </div>
+                                    <div class="col">
+                                        <div class="card-body">
+                                            <h5 class="card-title"> Ab Star Bulding</h5>
+                                            <h5 class="card-title">Megenagna</h5>
+                                            <p class="card-text">5000 birr/month</p>
+                                            <button class="btn btn-danger">GET CONTACT</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+
+                            </div>
+                        </div>
+                        <div class="col-md-1"></div>
+
+                        <div class="col-md-4">
+
+                            <div class="card">
+
+                                <div class="row">
+
+                                    <div class="col">
+                                        <img class="card-img-top h-100 w-100" src="assets/images/items/office.png" alt="Card image cap" />
+                                    </div>
+                                    <div class="col">
+                                        <div class="card-body">
+                                            <h5 class="card-title"> Ab Star Bulding</h5>
+                                            <h5 class="card-title">Megenagna</h5>
+                                            <p class="card-text">5000 birr/month</p>
+                                            <button class="btn btn-danger">GET CONTACT</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <a href="#">See All Featured</a>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <main class="col-md-9">
+                            {/* <header class="border-bottom mb-4 pb-3">
+                                <div class="form-inline">
+                                    <span class="mr-md-auto">32 Items found </span>
+                                    <select class="mr-2 form-control">
+                                        <option>Latest posts</option>
+                                        <option>Trending</option>
+                                        <option>Most Popular</option>
+                                        <option>Cheapest</option>
+                                    </select>
+                                    <div class="btn-group">
+                                        <a href="#" class="btn btn-outline-secondary active" data-toggle="tooltip" title="List view">
+                                            <i class="fa fa-bars"></i></a>
+                                        <a href="#" class="btn  btn-outline-secondary" data-toggle="tooltip" title="Grid view"> 
+                            <i class="fa fa-th"></i></a>
+                                    </div>
+                                </div>
+                            </header> */}
+
+                            <div class="row">
+                                <div class="col m-5"></div>
+                            </div>
+                            <div class="row ml-5 p-5">
+                                <div class="col-lg-4">
+                                    <a href="#"><p class="h3">Meeting Rooms |</p></a>
+                                </div>
+                                <div class="col-lg-4">
+                                <a href="#"><p class="h3">Office Rooms |</p></a>
+                                </div>
+                                <div class="col-lg-4">
+                                <a href="#"><p class="h3">Coworking Spaces</p></a>
+                                </div>
+                            </div>
+                            
+
+                            <div class="row">
+                                <div class="col-2"></div>
+                                <div class="col-10">
+                                <article class="card card-product-list">
+                                <div class="row no-gutters">
+                                    <aside class="col-md-3">
+                                        <a href="#" class="img-wrap">
+                                            <span class="badge badge-danger"> NEW </span>
+                                            <img src="assets/images/items/3.jpg" />
+                                        </a>
+                                    </aside>
+                                    <div class="col-md-6">
+                                        <div class="info-main">
+                                            <a href="#" class="h5 title"> Building name can be here  </a>
+
+                                            <p> Take it as demo specs, ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Ut wisi enim ad minim veniam </p>
+                                        </div>
+                                    </div>
+                                    <aside class="col-sm-3">
+                                        <div class="info-aside">
+                                            <div class="price-wrap">
+                                                <span class="price h5"> 140 ETB </span>
+                                                <del class="price-old"> 198 ETB</del>
+                                            </div>
+                                            <br />
+                                            <p>
+                                                <a href="#" class="btn btn-primary btn-block"> Get Contact </a>
+
+                                            </p>
+                                        </div>
+                                    </aside>
+                                </div>
+                            </article>
+                            <article class="card card-product-list">
+                                <div class="row no-gutters">
+                                    <aside class="col-md-3">
+                                        <a href="#" class="img-wrap"><img src="assets/images/items/4.jpg" /></a>
+                                    </aside>
+                                    <div class="col-md-6">
+                                        <div class="info-main">
+                                            <a href="#" class="h5 title"> Great Building name can be here  </a>
+
+
+                                            <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Ut wisi enim ad minim veniam </p>
+                                        </div>
+                                    </div>
+                                    <aside class="col-sm-3">
+                                        <div class="info-aside">
+                                            <div class="price-wrap">
+                                                <span class="price h5"> 56 ETB </span>
+                                                <del class="price-old"> 85 ETB</del>
+                                            </div>
+                                            <br />
+                                            <p>
+                                                <a href="#" class="btn btn-primary btn-block"> Get Contact </a>
+
+                                            </p>
+                                        </div>
+                                    </aside>
+                                </div>
+                            </article>
+
+                            <article class="card card-product-list">
+                                <div class="row no-gutters">
+                                    <aside class="col-md-3">
+                                        <a href="#" class="img-wrap"><img src="assets/images/items/5.jpg" /></a>
+                                    </aside>
+                                    <div class="col-md-6">
+                                        <div class="info-main">
+                                            <a href="#" class="h5 title"> Great Building name can be here  </a>
+
+
+                                            <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Ut wisi enim ad minim veniam </p>
+                                        </div>
+                                    </div>
+                                    <aside class="col-sm-3">
+                                        <div class="info-aside">
+                                            <div class="price-wrap">
+                                                <span class="price h5"> 56.00 ETB </span>
+                                            </div>
+                                            <br />
+                                            <p>
+                                                <a href="#" class="btn btn-primary btn-block"> Get Contact </a>
+
+                                            </p>
+                                        </div>
+                                    </aside>
+                                </div>
+                            </article>
+
+                            <article class="card card-product-list">
+                                <div class="row no-gutters">
+                                    <aside class="col-md-3">
+                                        <a href="#" class="img-wrap"><img src="assets/images/items/6.jpg" /></a>
+                                    </aside>
+                                    <div class="col-md-6">
+                                        <div class="info-main">
+                                            <a href="#" class="h5 title"> Office name can be here  </a>
+
+
+                                            <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Lorem ipsum dolor sit amet, consectetuer adipiscing elit, Ut wisi enim ad minim veniam </p>
+                                        </div>
+                                    </div>
+                                    <aside class="col-sm-3">
+                                        <div class="info-aside">
+                                            <div class="price-wrap">
+                                                <span class="price h5">62 ETB </span>
+                                            </div>
+                                            <br />
+                                            <p>
+                                                <a href="#" class="btn btn-primary btn-block"> Get Contact </a>
+
+                                            </p>
+                                        </div>
+                                    </aside>
+                                </div>
+                            </article>
+                            <nav class="mt-4" aria-label="Page navigation sample">
+                                <ul class="pagination">
+                                    <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                                </ul>
+                            </nav>
+                                </div>
+                                
+                             
+
+                            </div>
+
+                            
+                           
+                        </main>
+                    </div>
+                </div>
+            </section>
+
+            <footer class="section-footer border-top padding-y">
+                <div class="container">
+                    <p class="float-md-right">
+                        &copy; Copyright 2021 All rights reserved
+                    </p>
+                    <p>
+                        <a href="#">Terms and conditions</a>
+                    </p>
+                </div>
+            </footer>
+
+
+            </div>
+
+        </div>
+    );
+}
 export default App;
